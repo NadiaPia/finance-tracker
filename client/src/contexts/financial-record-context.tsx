069afedd15615e7061f58faff4f9dd35c2e36ca7
NadiaPia <206i417@gmail.com>
2024-05-10 20:04:00 -0700

    added table

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -1,8 +1,8 @@
 import { useUser } from "@clerk/clerk-react";
 import { createContext, useState, useContext, useEffect } from "react";
 
-interface FinancialRecord {
-  id?: string;
+export interface FinancialRecord {
+  _id?: string;
   userId: string;
   date: Date;
   description: string;
@@ -14,8 +14,8 @@ interface FinancialRecord {
 interface FinancialRecordsContextType {
   records: FinancialRecord[];
   addRecord: (records: FinancialRecord) => void;
-  // updateRecord: (id: string, newRecord: FinancialRecord) => void;
-  // deleteRecord: (id: string) => void;
+  updateRecord: (id: string, newRecord: FinancialRecord) => void;
+  deleteRecord: (id: string) => void;
 }
 
 export const FinancialRecordsContext = createContext<
@@ -65,8 +65,51 @@ export const FinancialRecordsProvider = ({
     }
   };
 
+  const updateRecord = async (id: string, newRecord: FinancialRecord) => {
+    
+    const response = await fetch(`http://localhost:3001/financial-records/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(newRecord),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    try {
+      if (response.ok) {
+        const newRecord = await response.json();
+        setRecords((prev) => prev.map((record) => {
+          if (record._id === id) {
+            return newRecord
+          } else {
+            return record;
+          }
+        }));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteRecord = async (id: string) => {
+    
+      const response = await fetch(`http://localhost:3001/financial-records/${id}`, {
+        method: "DELETE",        
+      });
+  
+      try {
+        if (response.ok) {
+          const deletedRecord = await response.json();
+          setRecords((prev) =>  prev.filter((record) => record._id !== deletedRecord._id));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    
+  }
+
+
+
   return (
-    <FinancialRecordsContext.Provider value={{ records, addRecord }}>
+    <FinancialRecordsContext.Provider value={{ records, addRecord,  updateRecord, deleteRecord}}>
       {children}
     </FinancialRecordsContext.Provider>
   );
